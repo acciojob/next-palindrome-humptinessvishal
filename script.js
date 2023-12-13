b277d63f274978279bd244ddd08d472de98cb437
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-12-13 20:03:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+function isPalindrome(num) {
+    return num.toString() === num.toString().split('').reverse().join('');
+}
+
 function nextPalindrome(num) {
   //your JS code here.
+	const inputNumber = parseInt(num);
+	if (!isNaN(inputNumber) && inputNumber > 0) {
+		let nextNum = inputNumber + 1;
+		while (!isPalindrome(nextNum)) {
+			nextNum++;
+		}
+		return nextNum;
+	} else {
+		alert("Please enter a valid positive integer.");
+	}
 }
 
 const input = prompt("Enter a palindrome number");
